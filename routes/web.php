57c1bd7ec2e71b5b1e311fe8993e281c0553f5ed
Ali Hammoud <alihammoud92754@gmail.com>
2024-12-25 02:29:53 +0200

    Blog - 1 - Module - relationships

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ThemeController;
@@ -20,6 +21,8 @@ Route::post('subscriber/store', [SubscriberController::class, 'store'])->name('s
 // CONTACT STORE ROUTE
 Route::post('contact/store', [ContactController::class, 'store'])->name('contact.store');
 
+// BLOG ROUTE
+Route::resource('blogs', BlogController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
